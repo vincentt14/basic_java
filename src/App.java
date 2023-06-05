41ef0487b63ec39a2432fad1e5e7ae1fe5191fea
Vincent <vincent14@student.umn.ac.id>
2023-06-05 23:02:41 +0700

    tes

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Bang!");
         System.out.println("udah makan?");
+        System.out.println("udah cok");
     }
 }
