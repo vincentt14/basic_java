cabcc9db2725c2fbd018ba725d64740cc385d797
Vincent <vincent14@student.umn.ac.id>
2023-06-06 21:19:03 +0700

    char, boolean, string

---
+++ src/TipeDataKarakter.java
@@ -0,0 +1,12 @@
+public class TipeDataKarakter {
+  public static void main(String[] args) {
+    char l = 'L';
+    char i = 'I';
+    char m = 'M';
+
+    System.out.print(l);
+    System.out.print(i);
+    System.out.print(m);
+
+  }
+}
---
+++ src/TipeDataString.java
@@ -0,0 +1,11 @@
+public class TipeDataString {
+  public static void main(String[] args) {
+    String firstName = "Vincent";
+    String lastName = "lim";
+    String fullName = firstName + " " + lastName;
+
+    System.out.println(firstName);
+    System.out.println(lastName);
+    System.out.println(fullName);
+  }
+}
