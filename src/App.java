6b8a5b6d7c39105c140d4b071caad30f2daaa0de
Vincent <vincent14@student.umn.ac.id>
2023-06-06 21:07:25 +0700

    number

--- src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Bang!");
-        System.out.println("udah makan?");
-        System.out.println("udah cok");
-    }
-}
---
+++ src/TipeDataNumber.java
@@ -0,0 +1,26 @@
+public class TipeDataNumber {
+  public static void main(String[] args) {
+
+    // bilangan bulat
+    byte iniByte = 100;
+    short iniShort = 1000;
+    int iniInt = 10000000;
+    long iniLong = 1000000000;
+    long iniLong2 = 1000000000L;
+
+    // bilangan desimal
+    float iniFloat = 10.10F;
+    double iniDouble = 10.10;
+
+    // literals
+    int decimalInt = 25;
+    int hexInt = 0xA132B;
+    int binInt = 0b01010101;
+
+    // underscore
+    long balance = 1_000_000_000L;
+    int sum = 60_000_000;
+
+    System.out.println(balance);
+  }
+}
