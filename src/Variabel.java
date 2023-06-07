86a2162d8dbc5a8461d296816c9731a500e4e2a9
Vincent <vincent14@student.umn.ac.id>
2023-06-07 19:20:33 +0700

    variabel

---
+++ src/Variabel.java
@@ -0,0 +1,28 @@
+public class Variabel {
+  public static void main(String[] args) {
+
+    String name;
+    name = "Vincent";
+    name = "bambang";
+
+    int age = 30;
+    String address = "Indonesia";
+
+    var manusia = "bababa";
+    var tidak = false;
+    var angka = 120;
+
+    final String bahasa = "Java bahasa ter hebat";
+    // bahasa = "Golang lebih hebat";
+
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(address);
+
+    System.out.println(manusia);
+    System.out.println(tidak);
+    System.out.println(angka);
+
+    System.out.println(bahasa);
+  }
+}
