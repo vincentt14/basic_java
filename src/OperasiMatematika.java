460499dbfa8323e26e69ca47e64844aea57ecde8
Vincent <vincent14@student.umn.ac.id>
2023-06-08 14:04:57 +0700

    operasi matematika

---
+++ src/OperasiMatematika.java
@@ -0,0 +1,29 @@
+public class OperasiMatematika {
+  public static void main(String[] args) {
+
+    int a = 10;
+    int b = 5;
+
+    System.out.println(a + b);
+    System.out.println(a - b);
+    System.out.println(a * b);
+    System.out.println(a / b);
+    System.out.println(a % b);
+
+    a *= 10;
+    System.out.println(a);
+
+    b *= 10;
+    System.out.println(b);
+
+    int tes = 5;
+    int tes2 = 5;
+
+    tes++;
+    System.out.println(tes);
+
+    tes2--;
+    System.out.println(tes2);
+
+  }
+}
