c219d916c84f7f9dea797288313ab596e9c96fce
Vincent <vincent14@student.umn.ac.id>
2023-06-08 14:16:22 +0700

    operasi perbandingan

---
+++ src/OperasiPerbandingan.java
@@ -0,0 +1,14 @@
+public class OperasiPerbandingan {
+  public static void main(String[] args) {
+
+    int a = 100;
+    int b = 200;
+
+    System.out.println(a > b);
+    System.out.println(a < b);
+    System.out.println(a >= b);
+    System.out.println(a <= b);
+    System.out.println(a == b);
+    System.out.println(a != b);
+  }
+}
