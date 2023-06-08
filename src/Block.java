3fe01dcd403eacb89199f54d49e171816a40d158
Vincent <vincent14@student.umn.ac.id>
2023-06-09 00:22:11 +0700

    block, statement, expression

---
+++ src/Block.java
@@ -0,0 +1,13 @@
+public class Block {
+  public static void main(String[] args) {
+
+    System.out.println("Halo bang");
+    System.out.println("Halo kak");
+    {
+      System.out.println("ini di dalam blok");
+      {
+        System.out.println("ini di dalam blok lagi");
+      }
+    }
+  }
+}
