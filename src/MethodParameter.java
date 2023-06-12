e0bc7ec2feffc322271dd1e6a96275e5fcfb16b9
Vincent <vincent14@student.umn.ac.id>
2023-06-12 21:51:47 +0700

    method parameter

---
+++ src/MethodParameter.java
@@ -0,0 +1,9 @@
+public class MethodParameter {
+  public static void main(String[] args) {
+    sayHello("bambang", 13);
+  }
+
+  static void sayHello(String name, int age) {
+    System.out.println("Halo " + name + " umur kamu " + age + " tahun.");
+  }
+}
